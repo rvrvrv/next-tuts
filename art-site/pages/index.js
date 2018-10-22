cbc39a99d60b267fcb1629e5ec79131c6e2059a9
rvrvrv <richvcode@gmail.com>
2018-10-22 11:40:17 -0500

    Display sample data

--- art-site/pages/index.js
+++ art-site/pages/index.js
@@ -1,7 +1,16 @@
 import Layout from '../components/Layout';
+import Photo from '../components/Photo';
+import getPhotos from '../data/data';
 
-export default () => (
+const Index = props => (
   <Layout>
-    <p>Inner Content</p>
+    {
+      props.images.map((image, i) => <Photo id={i} data={image} key={i} />)
+    }
   </Layout>
 );
+
+// Retrieve sample data (JSON)
+Index.getInitialProps = async () => ({ images: getPhotos() });
+
+export default Index;
