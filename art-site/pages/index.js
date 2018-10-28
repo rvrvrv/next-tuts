24a1d0d0b4247a4a3b738995cd473e91067d0347
rvrvrv <richvcode@gmail.com>
2018-10-28 11:07:54 -0500

    Begin building LikesEntry functionality

--- art-site/pages/index.js
+++ art-site/pages/index.js
@@ -15,11 +15,35 @@ export default class extends Component {
     });
   }
 
+  LikesEntry(id) {
+    const { images } = this.state;
+    const image = images.find(img => img.id === id);
+    image.likes += 1;
+    this.setState({
+      images
+    });
+    fetch(`http://localhost:4000/photos/${id}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(image)
+    });
+  }
+
   render() {
     return (
       <Layout>
         {
-          this.state.images.map((image, i) => <Photo id={i + 1} data={image} key={i} />)
+          this.state.images.map((image, i) => (
+            <Photo
+              id={i + 1}
+              data={image}
+              LikesEntry={this.LikesEntry.bind(this)}
+              key={i}
+            />
+          ))
         }
       </Layout>
     );
